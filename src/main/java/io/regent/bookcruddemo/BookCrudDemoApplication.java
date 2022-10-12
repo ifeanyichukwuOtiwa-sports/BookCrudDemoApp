72d44ec97ac31c589808b9808f6e9c4f5f453c1e
Ifeanyichukwu Otiwa <ifeanyichukwu.otiwa@betpawa.com>
2022-10-12 17:39:30 +0100

    Initial commit for book crud demo App

---
+++ src/main/java/io/regent/bookcruddemo/BookCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package io.regent.bookcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookCrudDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookCrudDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/regent/bookcruddemo/BookCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.regent.bookcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookCrudDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
